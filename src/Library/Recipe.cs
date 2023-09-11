b3a08ebb6230ef5d7e506f6d0154e5976dc70d65
JBevc <julian.bevc@correo.ucu.edu.uy>
2023-09-11 11:43:19 -0300

    Agregue responsabilidad

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -34,5 +34,26 @@ namespace Full_GRASP_And_SOLID.Library
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
         }
+
+        public double GetProductionCost()  
+         //Calcular el costo total de producir un poducto final
+        {
+            double totalSupplies = 0.0;
+            double totalEquipment = 0.0;
+            foreach (Step step in this.steps)
+            {
+                totalSupplies += step.Input.UnitCost;
+                totalEquipment += step.Equipment.HourlyCost*(step.Time/60.0);
+            }
+            double totalCost = totalSupplies + totalEquipment;
+            return totalCost;
+        }
     }
 }
+
+/* Para asignar esta responsabilidad, utilicé el patrón expert, que plantea que dbebemos asignar
+la responsabilidad a la clase que tiene la información necesaria para cumplir con la misma. 
+En este caso "Recipe" conoce los pasos y los productos involucrados en cada paso de la receta, 
+a partir de esto el método puede recorrer el Array privado de Recipe y calcular el costo total. 
+Como el Array es de tipo privado, únicamente se pueden acceder a esos datos utilizando 
+un metodo dentro de la clase Recipe y no en las otras. */
